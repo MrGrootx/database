84e0bedd18601547ad768cc5b0dac77671284891
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-16 20:43:50 +0530

    updated: last/first

--- server.lua
+++ server.lua
@@ -3,14 +3,17 @@ ESX = exports["es_extended"]:getSharedObject()
 
 ESX.RegisterCommand('newname', 'user', function(xPlayer, args, showError)
    -- xPlayer.triggerEvent('chat:clear')
-   senderId = xPlayer.getIdentifier()
+   print(args[1] .. " ".. args[2])
+   local senderId = xPlayer.getIdentifier()
    MySQL.Async.transaction(
       {
          -- table name [users] ,  particular row id firstname
-         'UPDATE users SET firstname = @name WHERE identifier = @senderId'
+         'UPDATE users SET firstname = @name WHERE identifier = @senderId',
+         'UPDATE users SET lastname = @lastname WHERE identifier = @senderId2'
       },
       {
-         ['name'] = args[1], ['senderId'] = senderId
+         ['name'] = args[1], ['senderId'] = senderId,
+         ['lastname'] = args[2], ['senderId2'] = senderId
       },
       function(success)
          print("Player" .. senderId .. "called now " .. args[1])
